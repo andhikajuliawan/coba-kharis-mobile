783de030370067392fb6816e6e8112716efd134f
andhikajuliawan <andhikajuliawan29@gmail.com>
2023-10-23 16:59:04 +0700

    update QR Code Scanner

--- src/Screen/QrCodeReaderScreen/QrCodeReaderScreen.tsx
+++ src/Screen/QrCodeReaderScreen/QrCodeReaderScreen.tsx
@@ -1,32 +1,109 @@
-import { Box, Divider, ScrollView, Text } from "native-base";
-
+import axios from 'axios';
+import { AlertDialog, Box, Button, Center, ScrollView, Text, Toast, View } from 'native-base';
+import React, { useContext, useState } from 'react';
+import { RNCamera } from 'react-native-camera';
+import QRCodeScanner from 'react-native-qrcode-scanner';
+import { BASE_URL } from '../../config';
+import { AuthContext } from '../../Context/AuthContext';
+import { Alert } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
-import { TouchableOpacity } from "react-native";
+import { TouchableOpacity } from 'react-native';
 
 const QrCodeReaderScreen = () => {
-    const navigation = useNavigation();
+  const navigation = useNavigation();
+  const { userInfo } = useContext(AuthContext);
+  const [isLoading, setIsLoading] = useState(false);
 
-    const Header = () => {
-        return (
-            <>
-                <Box bgColor="#fff" px={5} py={5}>
-                    <TouchableOpacity onPress={() => navigation.goBack()}>
-                        <Text>Back</Text>
-                    </TouchableOpacity>
-                </Box>
-                <Divider />
-            </>
+  // Alert Dialog
+  const [isOpen, setIsOpen] = React.useState(false);
+  const onClose = () => setIsOpen(false);
+  const cancelRef = React.useRef(null);
+  const [message, setMessage] = useState('');
 
-
-        )
-    }
-
+  const Header = () => {
     return (
-        <ScrollView>
-            <Header />
-            <Text>Halaman QrCodeReaderScreen</Text>
-        </ScrollView>
-    )
+      <View>
+        <TouchableOpacity onPress={() => navigation.goBack()}>
+          <Text>
+            Back
+          </Text>
+        </TouchableOpacity>
 
-}
+      </View>
+    )
+  }
+
+  const AbsensiQrCode = (data: any) => {
+    setIsLoading(true);
+    axios
+      .post(
+        `${BASE_URL}/api/create-peserta-presensi/`,
+        {
+          qrcode: data,
+          token: userInfo.token,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${userInfo.token}`,
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+          },
+        },
+      )
+      .then(response => {
+        // Toast.show({
+        //   description: `${response.data.message}`
+        // });
+
+        setMessage(response.data);
+        setIsOpen(!isOpen);
+      })
+      .catch(err => {
+        console.log(err);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  }
+
+  return (
+    <>
+      <Header />
+      <QRCodeScanner
+        onRead={({ data }) => AbsensiQrCode(data)}
+        // flashMode={RNCamera.Constants.FlashMode.torch}
+        reactivate={true}
+        reactivateTimeout={5000}
+      />
+      <Center>
+        <AlertDialog
+          leastDestructiveRef={cancelRef}
+          isOpen={isOpen}
+          onClose={onClose}>
+          <AlertDialog.Content>
+            <AlertDialog.CloseButton />
+            <AlertDialog.Header>
+              <Text></Text>
+            </AlertDialog.Header>
+            <AlertDialog.Body>
+              <Text>{message.message}</Text>
+            </AlertDialog.Body>
+            <AlertDialog.Footer>
+              <Box width="100%">
+                <TouchableOpacity>
+                  <Button
+                    bgColor="#003459"
+                    onPress={() => navigation.goBack()}>
+                    <Text color="#fff">kembali</Text>
+                  </Button>
+                </TouchableOpacity>
+              </Box>
+            </AlertDialog.Footer>
+          </AlertDialog.Content>
+        </AlertDialog>
+      </Center>
+    </>
+
+  );
+};
 export default QrCodeReaderScreen;
--- src/config.tsx
+++ src/config.tsx
@@ -1,3 +1,3 @@
 // export const BASE_URL = 'http://kharis.timapp.id';
-export const BASE_URL = 'http://192.168.1.2:8000';
+export const BASE_URL = 'http://192.168.1.7:8000';
 
